f310dd6a4d2b5eefd2bf2db169243db4053e23ae
Anton Kurenkov <quantum0@mail.ru>
2025-07-22 18:57:33 +0300

    Skip duplicates

--- routers/api/twitch_eventsub.py
+++ routers/api/twitch_eventsub.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging.config
+from uuid import UUID
 
 from fastapi import APIRouter, Security, Body, Depends, Path
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -22,6 +23,8 @@ router = APIRouter(prefix="/twitch")
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
 
+local_duplicates_cache: list[UUID] = []
+
 @router.post("/eventsub/{streamer_id}")
 async def eventsub_handler(
     payload: PointRewardRedemptionWebhookSchema | TwitchChallengeSchema,
@@ -35,6 +38,11 @@ async def eventsub_handler(
     if eventsub_message_type == "webhook_callback_verification":
         return PlainTextResponse(content=payload.challenge, media_type="text/plain")
 
+    # Skip duplicates
+    if payload.event.redemption_id in local_duplicates_cache:
+        logger.warning(f"Duplicated eventsub with redemption={payload.event.redemption_id}")
+        return Response(status_code=204)
+
     # Notification: channel points reward redemption
     logging.info(f"Notification from twitch: {payload}")
     result = await db.execute(
@@ -45,16 +53,16 @@ async def eventsub_handler(
     user = result.scalar_one_or_none()
 
     # Small sleep, maybe that will fix twitch's twitchAPI.types.TwitchResourceNotFound
-    await asyncio.sleep(3)
+    # await asyncio.sleep(3)
 
     if eventsub_message_type == "notification":
         try:
             result = await give_bonus(user.memealerts.memealerts_token, user.login_name, supporter=payload.event.user_input, amount=user.memealerts.coins_for_reward)
         except Exception as exc:
-            logger.error(exc)
+            logger.error(exc, exc_info=True)
             await chat_bot.send_message(user, "Непредвиденная ошибка начисления мемкоинов! О.О Баллы возвращены!")
             await twitch.cancel_redemption(user, payload.subscription.condition.reward_id, payload.event.redemption_id)
-            raise
+            pass
         if result:
             await chat_bot.send_message(user, "Мемкоины начислены :з")
             try:
@@ -70,4 +78,7 @@ async def eventsub_handler(
                 logger.error("Cannot find redemption")
                 pass
 
+    local_duplicates_cache.append(payload.event.redemption_id)
+    if len(local_duplicates_cache) > 30:
+        del local_duplicates_cache[0]
     return Response(status_code=204)
