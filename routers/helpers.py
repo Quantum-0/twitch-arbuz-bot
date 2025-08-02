3cb23c3904763e810c401f54ae2ae6da93569860
Anton Kurenkov <quantum0@mail.ru>
2025-08-02 06:46:27 +0300

    Minor fix

--- routers/helpers.py
+++ routers/helpers.py
@@ -18,7 +18,7 @@ SCHEMA_BY_TYPE: dict[str, type] = {
 async def parse_eventsub_payload(
     request: Request,
     eventsub_subscription_type: str = Header(..., alias="Twitch-Eventsub-Subscription-Type"),
-    _: str = Security(verify_eventsub_signature)
+    eventsub_message_type: str = Security(verify_eventsub_signature)
 ) -> PointRewardRedemptionWebhookSchema | RaidWebhookSchema | TwitchChallengeSchema:
     """
     Reusable dependency that parses the incoming EventSub payload and returns the appropriate schema.
@@ -28,7 +28,11 @@ async def parse_eventsub_payload(
     except Exception:
         raise HTTPException(status_code=400, detail="Invalid JSON body")
 
-    schema_cls = SCHEMA_BY_TYPE.get(eventsub_subscription_type)
+    if eventsub_message_type == "webhook_callback_verification":
+        schema_cls = TwitchChallengeSchema
+    else:
+        schema_cls = SCHEMA_BY_TYPE.get(eventsub_subscription_type)
+
     if not schema_cls:
         logger.warning(f"Determine schema {schema_cls} by eventsub_subscription_type: {eventsub_subscription_type}")
         logger.debug(body)
