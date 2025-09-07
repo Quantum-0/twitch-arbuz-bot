6ccaf330fab3be9b3b36ac014999853c661c955b
Anton Kurenkov <quantum0@mail.ru>
2025-09-07 21:41:32 +0300

    Add Admin API message to send message

--- routers/api/admin_api.py
+++ routers/api/admin_api.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Security, Depends, Query
+import sqlalchemy as sa
+from fastapi import APIRouter, Security, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.models import User
-from dependencies import get_db
-from routers.security_helpers import user_auth, admin_auth
-
-import sqlalchemy as sa
+from dependencies import get_db, get_twitch
+from routers.security_helpers import admin_auth
+from twitch.twitch import Twitch
 
 router = APIRouter(prefix="/admin", tags=["Admin API"])
 
@@ -19,3 +19,20 @@ async def update_settings(
     res = await db.execute(q)
     await db.commit()
     return {"success": res.rowcount}
+
+
+@router.post("/send_message")
+async def update_settings(
+    _: None = Security(admin_auth),
+    channel: str = Query(...),
+    message: str = Query(...),
+    db: AsyncSession = Depends(get_db),
+    twitch: Twitch = Depends(get_twitch),
+):
+    q = sa.select(User).where(User.login_name == channel)
+    res: User | None = await db.scalar(q)
+    if not res:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    await twitch.send_chat_message(res, message)
+    # return {"success": res.rowcount}
