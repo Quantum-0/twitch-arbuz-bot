d7250c0c5866e8efcb2eef89f33e210b64d7ab17
Anton Kurenkov <quantum0@mail.ru>
2026-06-04 22:29:09 +0300

    Minor fix redis: enable Egx

--- services/redis_state_manager.py
+++ services/redis_state_manager.py
@@ -267,6 +267,8 @@ class RedisStateManager(StateManager):
 
 async def init_redis(redis_url: str) -> AsyncGenerator[Redis, Any]:
     client = aioredis.from_url(redis_url, decode_responses=True)
+    # Важно: Включаем режим 'Ev' (gEneric + eXpired), чтобы ловить и DEL, и TTL
+    await client.config_set("notify-keyspace-events", "Egx")
     yield client
     await client.close()
 #
